a5ef5289c7f9bbc9cfc5f700166ad607434b6d7d
kevin dedaniya <kevin.dedaniya@gmail.com>
2020-09-02 12:55:42 +0530

    🎨 ✨ define Layout and add 404 view

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,29 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '',
+    name: 'Layout',
+    component: () => import('@/views/Layout.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: () => import('@/views/Home.vue'),
+      },
+      {
+        path: '*',
+        component: () => import('@/views/404.vue'),
+      },
+    ],
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
---
+++ src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    404 error
+  </div>
+</template>
+
+<script>
+export default {
+  name: '404',
+};
+</script>
